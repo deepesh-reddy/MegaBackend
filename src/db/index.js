02c4cc1474878c44b84f0f8c58cab817e0787a1f
Deepesh Reddy <deepeshreddy03@gmail.com>
2025-05-24 22:40:45 +0530

    schemas, bcrypt the password

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose'
-import { DB_NAME } from '../constants.js'
+import {
+    DB_NAME
+} from '../constants.js'
 import 'dotenv/config'
 
-const connectDB = async () =>{
+const connectDB = async () => {
     try {
+        console.log('Waiting for the database to connect, connection is in progress');
+
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
 
-        console.log(`\n MongoDB connected ! DB host : ${connectionInstance.connection.host}`);        
+        console.log(`\n MongoDB connected ! DB host : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log('MongoDB connection error', error);
         process.exit(1);
---
+++ src/routes/healthcheck.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import {healthCheck} from "../controllers/healthcheck.controllers.js"
+
+const router = Router()
+
+// router.route('/').get(healthCheck)
+router.get('/', healthCheck);
+
+
+export default router;
